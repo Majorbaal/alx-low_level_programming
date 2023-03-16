2efeec8ebab9db89952f785ee9ee2eba5551c9f5
Majorbaal <hamzyss81@gmail.com>
2023-03-16 15:52:50 -0700

    5print

--- 0x00-hello_world/5printf.c
+++ 0x00-hello_world/5printf.c
@@ -1,11 +1,10 @@
-#!/bin/bash
 #include <stdio.h>
 /**
  * Description: main - prints out a string to stdout.
  * Return: 0 if success.
  */
 int main(void)
-(
- puts("\"Programming is like building a multilingual puzzle");
- return(0);
- )
+{
+printf("with proper grammar, but the outcome is a piece of art,\n:);
+return (0);
+}
