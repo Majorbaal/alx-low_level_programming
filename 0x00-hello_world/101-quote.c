e24a6520db9ebf930a5d23862ecdf26edda863e1
Majorbaal <hamzyss81@gmail.com>
2023-03-17 12:56:37 -0700

    101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <unlstd.h>
+/**
+ * main - Entry Point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",
+printf("and that piece of art is useful\" - Dora Korpar, 2015-10-19));
+return (1);
+}
