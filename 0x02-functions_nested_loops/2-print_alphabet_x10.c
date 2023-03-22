494a8bf9a18ad78ad18a03acdb2845620e029b07
Majorbaal <hamzyss81@gmail.com>
2023-03-22 03:58:33 -0700

    2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -2,6 +2,7 @@
 
 /**
  * print_alphabet_x10 - print 10 times the alphabet, in lowercase
+ *
  */
 
 void print_alphabet_x10(void)
@@ -15,3 +16,4 @@ void print_alphabet_x10(void)
 			_putchar(letter);
 		_putchar('\n');
 	}
+}
