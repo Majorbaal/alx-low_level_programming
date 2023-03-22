16cc2a3c138ffacdbb220e293bdc7bdd9a93e93e
Majorbaal <hamzyss81@gmail.com>
2023-03-22 03:43:55 -0700

    2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -3,6 +3,7 @@
 /**
  * print_alphabet_x10 - print 10 times the alphabet, in lowercase
  */
+
 void print_alphabet_x10(void)
 {
 	int count = 0;
@@ -10,7 +11,7 @@ void print_alphabet_x10(void)
 
 	while (count++ <= 9)
 	{
-		for letter = ('a'; letter <= 'z'; letter ++)
+		for letter = ('a'; letter <= 'z'; letter++)
 		_putchar(letter);
-	_putchar('\n');
+	_putchar('/n');
 	}
