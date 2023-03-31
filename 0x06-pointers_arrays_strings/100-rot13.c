0040631f9dd88cae7a2a1fe1fbc63f0abe4a3e4a
Majorbaal <hamzyss81@gmail.com>
2023-03-31 14:46:05 -0700

    m

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -1,11 +1,13 @@
 #include "main.h"
 #include <stdio.h>
+
 /**
  * rot13 - encoder rot13
  * @s: pointer to string
  *
  * Return: *s
  */
+
 char *rot13(char *s)
 {
 	int i;
@@ -15,7 +17,7 @@ char *rot13(char *s)
 
 	for (i = 0; s[i] != '\0'; i++)
 	{
-		for (j = 0; j < 52; j++)
+		for (j - 0; j < 52; j++)
 		{
 			if (s[i] == data1[j])
 			{
